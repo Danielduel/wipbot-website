a51f35ea22f50c253d5c338bd1ec7f1e31a9102a
Daniel Mizerski <danielduel-github@dduel.dev>
2025-07-29 17:03:36 +0200

    handle downloading wips

--- routes/wips/[wipcode].ts
+++ routes/wips/[wipcode].ts
@@ -1,6 +1,15 @@
 import { FreshContext } from "$fresh/server.ts";
+import { S3Client } from "../../process/s3Client.ts";
 
-export const handler = (_req: Request, _ctx: FreshContext): Response => {
-  const wipcode = _ctx.params.wipcode;
-  return new Response(`${wipcode}`);
+export const handler = async (_req: Request, _ctx: FreshContext): Promise<Response> => {
+  const _wipcode = _ctx.params.wipcode;
+  const wipcode = _wipcode.substring(1);
+
+  console.log(_wipcode, " -> ", wipcode);
+  const s3Client = S3Client.getS3Client();
+  const exists = await s3Client.exists(wipcode, { bucketName: S3Client.BUCKET.WIP_BLOB });
+  if (!exists) throw 404;
+
+  const data = await s3Client.getObject(wipcode, { bucketName: S3Client.BUCKET.WIP_BLOB }); 
+  return data;
 };
